b9d05bad6e3ff2cf166ffb8de629a0bfa7f74411
Sushanth <sushanthchuchu7@gmail.com>
2024-07-05 17:26:19 +0530

    #Set Repository

---
+++ app/src/main/java/com/example/noteup/ui/repository/NotesRepository.kt
@@ -0,0 +1,32 @@
+package com.example.noteup.ui.repository
+
+import android.provider.ContactsContract.CommonDataKinds.Note
+import com.example.noteup.ui.dao.NotesDao
+
+class NotesRepository(val notedao:NotesDao) {
+
+    //create Note
+    suspend fun createNote(note:Note){
+        notedao.insertNote(note)
+    }
+
+    //getallnote
+
+    fun getAllNotes(){
+        notedao.getNotes()
+    }
+
+    //update notes
+    suspend fun updateNote(note:Note){
+        notedao.updateNote(note)
+    }
+
+    //delete note
+
+    suspend fun deleteNote(note:Note){
+        notedao.deleteNote(note)
+    }
+
+
+
+}
